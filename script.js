4ae16f623043e93e662dccb5977f14655e2a2eb9
Yash VIjay Kumbhkarn <cse230001083@iiti.ac.in>
2024-10-10 19:23:38 +0530

    finish

--- script.js
+++ script.js
@@ -1,11 +1,39 @@
 document.addEventListener('DOMContentLoaded', function () {
-    // Typing animation for the header subtitle
     const typingElement = document.querySelector('.typing');
     const phrases = ['Android Developer', 'ML Enthusiast', 'Tech Explorer'];
     let phraseIndex = 0;
     let letterIndex = 0;
     let currentText = '';
+    const scrollableSection = document.querySelector('.scrollable-section');
+    let isDown = false;
+    let startX;
+    let scrollLeft;
+    const cards = document.querySelectorAll('.card');
     
+    scrollableSection.addEventListener('mousedown', (e) => {
+      isDown = true;
+      scrollableSection.classList.add('active');
+      startX = e.pageX - scrollableSection.offsetLeft;
+      scrollLeft = scrollableSection.scrollLeft;
+    });
+  
+    scrollableSection.addEventListener('mouseleave', () => {
+      isDown = false;
+      scrollableSection.classList.remove('active');
+    });
+  
+    scrollableSection.addEventListener('mouseup', () => {
+      isDown = false;
+      scrollableSection.classList.remove('active');
+    });
+  
+    scrollableSection.addEventListener('mousemove', (e) => {
+      if (!isDown) return;
+      e.preventDefault();
+      const x = e.pageX - scrollableSection.offsetLeft;
+      const walk = (x - startX) * 2;
+      scrollableSection.scrollLeft = scrollLeft - walk;
+    });
     function type() {
       if (letterIndex < phrases[phraseIndex].length) {
         currentText += phrases[phraseIndex][letterIndex];
@@ -16,7 +44,7 @@ document.addEventListener('DOMContentLoaded', function () {
         setTimeout(erase, 2000);
       }
     }
-    
+  
     function erase() {
       if (letterIndex > 0) {
         currentText = currentText.slice(0, -1);
@@ -28,7 +56,5 @@ document.addEventListener('DOMContentLoaded', function () {
         setTimeout(type, 500);
       }
     }
-    
-    type();
-  });
-  
+  type();
+});
